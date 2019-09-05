5705dfdef8cacde5065e12df37756277beddaf9b
César Blanco <cesarfbg10@gmail.com>
2019-09-05 16:51:18 -0500

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { BuyTicketButtonComponent } from './components/buy-ticket-button/buy-ticket-button.component';
+import { MapComponent } from './components/map/map.component';
+import { TicketsComponent } from './components/tickets/tickets.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    BannerComponent,
+    BuyTicketButtonComponent,
+    MapComponent,
+    TicketsComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/tickets/tickets.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tickets',
+  templateUrl: './tickets.component.html',
+  styleUrls: ['./tickets.component.css']
+})
+export class TicketsComponent {
+
+  ticketText1 = '¿Aún no decides que día ir?';
+  ticketText2 = 'Asegura tu visita el día que quieras con el pase Free Day';
+
+}
