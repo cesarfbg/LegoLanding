0cfc45fa523e0b59c0407e57b0c2146d7ca53289
César Blanco <cesarfbg10@gmail.com>
2019-09-09 16:15:00 -0500

    Textos LEGO modificados

--- src/app/components/schedules/schedules.component.ts
+++ src/app/components/schedules/schedules.component.ts
@@ -11,7 +11,7 @@ export class SchedulesComponent {
     ['Construcción Monocromática', 'station01.jpg', false],
     ['Zona de desafíos', 'station01.jpg', false],
     ['Crea tu País', 'station01.jpg', false],
-    ['Lego Mike', 'station01.jpg', false],
+    ['LEGO® Mike', 'station01.jpg', false],
     ['Galería de arte', 'station01.jpg', false],
     ['Ninjago', 'station01.jpg', false],
     ['Rampa de carreras', 'station01.jpg', false],
@@ -20,15 +20,15 @@ export class SchedulesComponent {
   ];
 
   stations2 = [
-    ['Lego friends', 'station01.jpg', false],
-    ['Lego city', 'station01.jpg', false],
+    ['LEGO® friends', 'station01.jpg', false],
+    ['LEGO® city', 'station01.jpg', false],
     ['Lago duplo', 'station01.jpg', false],
-    ['Modelos lego', 'station01.jpg', false],
+    ['Modelos LEGO®', 'station01.jpg', false],
     ['Photo Opportunity Harry Potter', 'station01.jpg', false],
     ['Casa papás perdidos', 'station01.jpg', false],
-    ['Mar lego', 'station01.jpg', false],
+    ['Mar LEGO®', 'station01.jpg', false],
     ['Building Happening', 'station01.jpg', false],
-    ['Tienda Lego', 'station01.jpg', false]
+    ['Tienda LEGO®', 'station01.jpg', false]
   ];
 
   show( idx, arr ) {
