e7e9ec8a970d2a09064c7e611ce436422d1ce460
César Blanco <cesarfbg10@gmail.com>
2019-09-11 16:53:17 -0500

    agregados multiples efectos visuales, agregada foto a Building Happening, cambios menores en tamaños de texto y contenido de párrafos.

--- src/app/components/schedules/schedules.component.ts
+++ src/app/components/schedules/schedules.component.ts
@@ -12,7 +12,7 @@ export class SchedulesComponent {
     ['Construcción Monocromática', 'Estacion_ConstruccionMonocromatica.jpg', 'Ven y realiza fantásticas construcciones en violeta y complementa las de otros participantes si quieres. ¡Te sorprenderás con el resultado!', false, true],
     ['Crea tu País', 'Estacion_CreaPais.jpg', '¿Cómo crearías a Colombia si tuvieras la posibilidad de construirla con ladrillos LEGO®? Construye lo que piensas que representa a tu país y ubícalo en la región que desees dentro del mapa.', false, true],
     ['Galería de arte', 'Estacion_GaleriaArte.jpg', 'Saca a relucir tu artista interior, utilizando una amplia gama de ladrillos de colores LEGO®, con los que podrás realizar las obras más creativas para luego exhibirlas dentro de nuestra galería.  Podrás contar con la ayuda de nuestro crítico de arte si lo deseas.', false, true],
-    ['Photo Opportunity Harry Potter™', 'Estacion_HarryPotter.jpg', 'Disfrázate de tus personajes favoritos de Harry Potter™, tómate una foto con ellos y llévate un recuerdo de esta gran aventura. Vive la experiencia al saber qué dice el sombrero seleccionador sobre tu casa en Hogwarts (Gryffindor, Hufflepuff, Ravenclaw o Slytherin).', false, true],
+    ['Photo Opportunity Harry Potter™', 'Estacion_HarryPotter.jpg', 'Disfrázate de tus personajes favoritos de Harry Potter™, tómate una foto con ellos y llévate un recuerdo de esta gran aventura.', false, true],
     ['Lago duplo', 'Estacion_LagoDuplo.jpg', 'En este gran lago de Duplo™, lo más pequeños podrán dejar volar su imaginación con los ladrillos de LEGO® diseñados especialmente para ellos.', false, true],
     ['LEGO® city', 'Estacion_LegoCity.jpg', 'Crea las más divertidas aventuras en esta ciudad, ayudándonos a resolver diferentes misiones.', false, true],
     ['LEGO® friends', 'Estacion_LegoFriends.jpg', 'Da un gran concierto para tus amigos como un artista, con los accesorios que encontrarás en el camerino. Además, podrás construir con Andrea, Mia, Emma, Olivia y Stephanie nuevas aventuras dentro de Harley City.', false, true],
@@ -27,8 +27,8 @@ export class SchedulesComponent {
     ['Rampas de Carreras', 'Estacion_Rampas.jpg', 'Construye el vehículo más veloz en la zona de PITS y pon a prueba su desempeño y resistencia en las increíbles rampas. Si quedas entre los tres primeros subirás a celebrar en el podio. En sus marcas, ¿listos? ¡Fuera!', false, true],
     ['Star Wars™', 'Estacion_StarWars.jpg', 'Intercepta al enemigo y junto a tu equipo construye una increíble nave a toda velocidad, ¡Deprisa! no cuentas con mucho tiempo.', false, true],
     ['Zona de Desafíos', 'Estacion_ZonadeDesafios.jpg', 'El tiempo corre y tienes pocos minutos para construir la creación de LEGO® que te indicará nuestro explorador. ', false, true],
-    ['Sala LEGO®', '', 'Este espacio está equipado para mantener a tus papás a salvo mientras los encuentras.', false, true],
-    ['Building Happening', '', 'Siguiendo las instrucciones del guía, vas a construir partes de este camión de bomberos, así entre todos vamos a hacer el camión más grande que haya existido.', false, true]
+    ['Building Happening', 'Estacion_BuildingHappening.jpg', 'Siguiendo las instrucciones del guía, vas a construir partes de este camión de bomberos, así entre todos vamos a hacer el camión más grande que haya existido.', false, true],
+    ['Sala LEGO®', '', 'Este espacio está equipado para mantener a tus papás a salvo mientras los encuentras.', false, true]
   ];
 
   show( idx, arr ) {
